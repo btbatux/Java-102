9b877407bf58adf33d9b0dfc9afc7c220182851f
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-04-12 16:27:56 +0300

    NotepadUygulamasi
    NotepadUygulamasi

---
+++ NotepadUygulaması/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\pc\\IdeaProjects\\untitled\\src\\notlar.txt";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir metin girin: ");
+        String metin = scanner.nextLine();
+
+        MetinYazdir.dosyayaYazdir(metin, path);
+        MetinOku.metinOku(path);
+
+    }
+}
